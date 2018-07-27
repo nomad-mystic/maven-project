cc868bcb432e3984e138bf8ffad5693a8d4bb229
kmurphy <kmurphy@casenetllc.com>
2018-07-27 18:15:26 -0400

    master: Simple change to class.

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,11 @@ public class Greeter {
 
   }
 
+  public void host() {
+    System.out.println("This will be a host method");
+  }
+
+
   //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
